207e422a85bfce6e5feead33e260a0f7cb3f7da8
chenpengxiang <chenpengxiang@dianxiaomi.com>
2024-06-03 20:32:59 +0800

    feat: 修改redis 用户缓存数据

--- src/apps/auth/auth.service.ts
+++ src/apps/auth/auth.service.ts
@@ -19,6 +19,7 @@ import { LoggerService } from 'src/shared/logger/logger.service'
 import { TokenPayload, TokenService } from 'src/shared/token.service'
 import { UserCacheModel } from 'src/common/models/user-cache.model'
 import { UserPayload } from '../../types/prisma'
+import { Permission, PermissionCode } from 'src/constant/permission.constant'
 
 @Injectable()
 export class AuthService {
@@ -97,11 +98,14 @@ export class AuthService {
             throw new BusinessException(ErrorEnum.INVALID_USERNAME_PASSWORD)
         }
 
+        const permissionCode = this.getPermissionCode(user)
+
         const payload = {
             account: user.account,
             level: user.level,
             id: user.id,
             nickname: user.nickname,
+            permissionCode
         }
         const token = await this.tokenService.generateToken(payload)
 
@@ -115,17 +119,41 @@ export class AuthService {
         return token
     }
 
-    private setUserCache(param: {
+    private getPermissionCode(user: UserPayload) {
+        if (!user.role) {
+            return []
+        }
+
+        const permissionList: PermissionCode[] = []
+
+        function traverse(list: unknown[]) {
+            list.forEach((item) => {
+                const { selected, code, children } = item as Permission
+
+                if (selected) {
+                    permissionList.push(code)
+                }
+                if (children) {
+                    traverse(children)
+                }
+            })
+        }
+        traverse(user.role.permissions)
+
+        return permissionList
+    }
+
+    private setUserCache(params: {
         ip: string
         ua: string
         accessToken: string
         user: UserPayload
         refreshToken: string
     }) {
-        const userCacheData = new UserCacheModel(param)
+        const userCacheData = new UserCacheModel(params)
         const {
             user: { account },
-        } = param
+        } = params
 
         const cacheKey = getUserCacheKey(account)
 
--- src/common/models/user-cache.model.ts
+++ src/common/models/user-cache.model.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { UserLevel } from '@prisma/client'
 import { UserPayload } from '../../types/prisma'
-import { Permission, PermissionCode } from '../../constant/permission.constant'
 
 interface UserCacheData {
     user: UserPayload
@@ -18,8 +17,8 @@ export class UserCacheModel {
     @ApiProperty({ type: UserLevel, description: '用户等级' })
     userLevel: UserLevel
 
-    @ApiProperty({ type: 'string', description: '用户角色, 使用时需要JSON.parse解析为数组' })
-    userRole: string
+    @ApiProperty({ type: 'string', description: '用户角色ID' })
+    roleId: number
 
     @ApiProperty({ type: 'string', description: '登录时用户IP' })
     ip: string
@@ -35,7 +34,7 @@ export class UserCacheModel {
 
     constructor(userCacheData: UserCacheData) {
         const { user, ip, ua, refreshToken, accessToken } = userCacheData
-        const { account, level } = user
+        const { account, level, roleId } = user
 
         this.account = account
         this.userLevel = level
@@ -43,30 +42,8 @@ export class UserCacheModel {
         this.ua = ua
         this.refreshToken = refreshToken
         this.accessToken = accessToken
-        this.userRole = this.getPermissionCode(user)
+        this.roleId = roleId
     }
 
-    private getPermissionCode(user: UserPayload) {
-        if (!user.role) {
-            return JSON.stringify([])
-        }
 
-        const permissionList: PermissionCode[] = []
-
-        function traverse(list: unknown[]) {
-            list.forEach((item) => {
-                const { selected, code, children } = item as Permission
-
-                if (selected) {
-                    permissionList.push(code)
-                }
-                if (children) {
-                    traverse(children)
-                }
-            })
-        }
-        traverse(user.role.permissions)
-
-        return JSON.stringify(permissionList)
-    }
 }
